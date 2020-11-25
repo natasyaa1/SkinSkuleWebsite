f13c7894f04456fe84fdd30a2d3c55f7ecf023e3
natasyaa1 <audrey.natasya@mail.utoronto.ca>
2020-11-25 18:43:53 -0500

    First Commit

---
+++ src/com/mie/controller/SignupController.java
@@ -0,0 +1,95 @@
+package com.mie.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mie.model.*;
+import com.mie.dao.*;
+
+/**
+ * Servlet implementation for LoginController.
+ * 
+ * This class handles the login servlet and assigns session attributes for users
+ * who succesfully log into the system.
+ */
+public class SignupController extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+
+		/**
+		 * Retrieve the entered email and password from the signup.jsp form.
+		 */
+		Member member = new Member();
+		member.setName(request.getParameter("n"));
+		member.setEmail(request.getParameter("em"));
+		member.setPassword(request.getParameter("pw"));
+		member.setSkinType_ID(request.getParameter("sk"));
+		
+		MemberDao.adduser(member);
+
+		try {
+			//Checks if email is unique
+			/**
+			 * Try to see if the member exist.
+			 */
+			boolean uniqueemail = MemberDao.checkemail(member);
+			
+			//If email already exists in database, ask them to login instead or redirect to login page
+			
+			/**
+			 * If the isValid value is true, assign session attributes to the
+			 * current member.
+			 */
+			if (uniqueemail) {
+
+//				HttpSession session = request.getSession(true);
+//				session.setAttribute("currentSessionmember", member);
+//				session.setAttribute("username", member.getUsername());
+//				session.setAttribute("firstname", member.getFirstName());
+//				session.setAttribute("lastname", member.getLastName());
+				/**
+				 * Redirect to the login page.
+				 */
+				response.sendRedirect("login.jsp");
+
+				/**
+				 * Set a timeout variable of 900 seconds (15 minutes) for this
+				 * member who has logged into the system.
+				 */
+				//session.setMaxInactiveInterval(900);
+			}
+			
+			//If email is unique, 
+				//check if email is in an email form e.g. has an @, no spaces
+				//check password requirements are met
+			
+			
+			
+
+			
+			
+			
+
+			else {
+				/**
+				 * Otherwise, redirect the user to the invalid login page and
+				 * ask them to log in again with the proper credentials.
+				 */
+				response.sendRedirect("login.jsp");
+			}
+		}
+
+		catch (Throwable theException) {
+			/**
+			 * Print out any errors.
+			 */
+			System.out.println(theException);
+		}
+	}
+}
---
+++ src/com/mie/dao/MemberDao.java
@@ -0,0 +1,168 @@
+package com.mie.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mie.util.DbUtil;
+import com.mie.model.*;
+import com.mie.controller.*;
+import com.mie.util.*;
+
+public class MemberDao {
+
+	/**
+	 * This class handles the Member objects and the login component of the web
+	 * app.
+	 */
+	static Connection currentCon = null;
+	static ResultSet rs = null;
+
+	public static Member login(Member member) {
+
+		/**
+		 * This method attempts to find the member that is trying to log in by
+		 * first retrieving the username and password entered by the user.
+		 */
+		Statement stmt = null;
+
+		String email = member.getEmail();
+		String password = member.getPassword();
+
+		/**
+		 * Prepare a query that searches the members table in the database
+		 * with the given username and password.
+		 */
+		String searchQuery = "select * from Users where Email_Address='"
+				+ email + "' AND Password='" + password + "'";
+
+		try {
+			// connect to DB
+			currentCon = DbUtil.getConnection();
+			stmt = currentCon.createStatement();
+			rs = stmt.executeQuery(searchQuery);
+			boolean more = rs.next();
+
+			/**
+			 * If there are no results from the query, set the member to false.
+			 * The person attempting to log in will be redirected to the home
+			 * page when isValid is false.
+			 */
+			
+			if (!more) {
+				member.setValid(false);
+			}
+
+			/**
+			 * If the query results in an database entry that matches the
+			 * username and password, assign the appropriate information to
+			 * the Member object.
+			 */
+			else if (more) {
+				String Name = rs.getString("Name");
+
+				member.setName(Name);
+				member.setValid(true);
+			}
+		}
+
+		catch (Exception ex) {
+			System.out.println("Log In failed: An Exception has occurred! "
+					+ ex);
+		}
+		/**
+		 * Return the Member object.
+		 */
+		return member;
+
+	}
+	
+	public static boolean checkemail(Member member) {
+
+		/**
+		 * This method attempts to find the member that is trying to log in by
+		 * first retrieving the username and password entered by the user.
+		 */
+		Statement stmt = null;
+
+		String email = member.getEmail();
+
+		/**
+		 * Prepare a query that searches the members table in the database
+		 * with the given username and password.
+		 */
+		String searchQuery = "select * from members where email='"
+				+ email + "'";
+
+		try {
+			// connect to DB
+			currentCon = DbUtil.getConnection();
+			stmt = currentCon.createStatement();
+			rs = stmt.executeQuery(searchQuery);
+			boolean more = rs.next();
+
+			/**
+			 * If there are no results from the query, set the boolean to true.
+			 * 
+			 * 
+			 */
+			
+			if (!more) {
+				
+				return true;
+				//member.setValid(false);
+			}
+
+			/**
+			 * If the query results in an database entry that matches an
+			 * email in the database, set the boolean to false
+			 * ask the user to login instead
+			 */
+			else if (more) {
+//				String firstName = rs.getString("FirstName");
+//				String lastName = rs.getString("LastName");
+//
+//				member.setFirstName(firstName);
+//				member.setLastName(lastName);
+//				member.setValid(true);
+				return false;
+			}
+		}
+
+		catch (Exception ex) {
+			System.out.println("Log In failed: An Exception has occurred! "
+					+ ex);
+		}
+		/**
+		 * Return the Member object.
+		 */
+		//return member;
+		return false;
+
+
+	}
+	public static void adduser(Member member) {
+		Statement stmt = null;
+		//Integer user_id = member.getUser_ID();
+		String name = member.getName();
+		String email = member.getEmail();
+		String password = member.getPassword();
+		String skintype_id = member.getSkinType_ID();
+
+		/**
+		 * Prepare a query that searches the members table in the database
+		 * with the given username and password.
+		 */
+		String searchQuery = "insert into users(Name, Email_Address, Password, SkinType_ID) "
+				+ "VALUES (" + name + ", " +
+          email + ", " + password + ", " + skintype_id + ")";
+				
+		
+		
+	}
+	
+}
